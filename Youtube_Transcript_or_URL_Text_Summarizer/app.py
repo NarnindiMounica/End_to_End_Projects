e2a33e2259b8fe8ca2a197694699c26d99a32579
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-03-22 14:44:25 +0530

    importing libraries

--- Youtube_Transcript_or_URL_Text_Summarizer/app.py
+++ Youtube_Transcript_or_URL_Text_Summarizer/app.py
@@ -2,5 +2,11 @@ import os
 import streamlit as st
 import validator
 from langchain_groq import ChatGroq
-from langchain import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_classic.chains import load_summarize_chain
+
+
+st.title("➡️Summarize Youtube Video Transcript or URL🌍")
+
