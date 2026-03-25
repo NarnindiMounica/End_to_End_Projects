574b7a902f7a7859b680b4e9429d709c1ed58e99
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-03-25 23:37:14 +0530

    local db creation is completed

--- Chat_With_SQL_DB/local_db.py
+++ Chat_With_SQL_DB/local_db.py
@@ -6,6 +6,9 @@ connection = sqlite3.Connection("students.db")
 #creating the cursor object
 cursor = connection.cursor()
 
+#if table already exists, delete it to create new one
+cursor.execute('''drop table if exists student''')
+
 #sql query to create a table
 
 table_info="""
@@ -20,3 +23,18 @@ table_info="""
 
 #creating a table
 cursor.execute(table_info)
+
+#inserting records into table
+
+cursor.execute('''insert into student values ("Mounica", 100, "Data Science", "A", 98)''')
+cursor.execute('''insert into student values ("Ram", 101, "Agentic AI", "A", 78)''')
+cursor.execute('''insert into student values ("Sammy", 102, "Data Science", "A", 67)''')
+cursor.execute('''insert into student values ("John", 103, "Deep Learning", "A", 89)''')
+cursor.execute('''insert into student values ("Amar", 104, "Agentic AI", "A", 70)''')
+
+rows = cursor.execute('''select * from student''')
+
+#displaying records
+
+for row in rows:
+    print(row)
