4d80b1170a7ab1e4c95d06bd5c09b13359c86175
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-03-25 23:26:41 +0530

    initial commit

--- Chat_With_SQL_DB/local_db.py
+++ Chat_With_SQL_DB/local_db.py
@@ -3,5 +3,20 @@ import sqlite3
 #connecting to local db "students.db"
 connection = sqlite3.Connection("students.db")
 
-#creating the cursor
+#creating the cursor object
 cursor = connection.cursor()
+
+#sql query to create a table
+
+table_info="""
+    create table student
+    (
+    student_name varchar(25),
+    student_id int,
+    class varchar(25),
+    section varchar(25),
+    marks int
+    )"""
+
+#creating a table
+cursor.execute(table_info)
