2c5e6b2031dc393a7a724ab98d767aa17e3599da
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-03-27 23:49:24 +0530

    model called

--- Chat_With_SQL_DB/sql_agent.py
+++ Chat_With_SQL_DB/sql_agent.py
@@ -1,8 +1,14 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
-
+from langchain.chat_models import init_chat_model
 
 
 load_dotenv()
 
 groq_api_key=os.getenv("GROQ_API_KEY")
+
+st.title("SQL Agent")
+
+#llm model
+llm = init_chat_model(model="groq:llama-3.1-8b-instant", api_key=groq_api_key)
