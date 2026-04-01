014e294ba6f093d18bf99459fe6280fa82631d66
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-04-01 23:33:27 +0530

    simple bot node definition

---
+++ Web_Search_Functionality_Chatbot/src/web_search_agentic_bot/nodes/simple_bot_node.py
@@ -0,0 +1,12 @@
+
+
+class SimpleBotNode:
+
+    def __init__(self, state, model):
+        self.state = state
+        self.model = model
+
+    def get_simple_bot_node(self):
+        model_response = self.model.invoke(self.state['messages'])
+        return {self.state['messages']: model_response}
+           
