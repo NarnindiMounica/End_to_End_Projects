5a150d71920509e6865ff6bd9166f0138a2b3f52
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-04-02 21:12:44 +0530

    display ui class is completed

---
+++ Web_Search_Functionality_Chatbot/src/web_search_agentic_bot/ui/streamlitui/displayui.py
@@ -0,0 +1,24 @@
+import os
+import streamlit as st
+
+class DisplayStreamlitUI:
+    def __init__(self, graph, usecase, user_message):
+        self.graph = graph
+        self.usecase = usecase
+        self.user_message = user_message
+
+    def display_streamlit_ui(self):
+        graph = self.graph
+        usecase = self.usecase
+        user_message = self.user_message
+
+        if usecase.lower()=="generic search":
+            for event in graph.stream({'messages': user_message}, stream_mode="values"):
+                print(event.values())
+
+            with st.chat_message("user"):
+                st.write(user_message)
+            with st.chat_message("assistant"):
+                st.write(event['messages'][-1].content)  
+
+
