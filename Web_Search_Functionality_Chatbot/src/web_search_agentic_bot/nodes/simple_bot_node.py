b037228489d823edd1afb1cb625dcf0f11fa3059
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-04-02 20:19:58 +0530

    simple bot node function

--- Web_Search_Functionality_Chatbot/src/web_search_agentic_bot/nodes/simple_bot_node.py
+++ Web_Search_Functionality_Chatbot/src/web_search_agentic_bot/nodes/simple_bot_node.py
@@ -2,11 +2,11 @@ from src.web_search_agentic_bot.states.simple_state import SimpleState
 
 class SimpleBotNode:
 
-    def __init__(self, state, model):
-        self.state = state
+    def __init__(self, model):
+        
         self.model = model
 
-    def get_simple_bot_node(self):
-        model_response = self.model.invoke(self.state['messages'])
-        return {self.state['messages']: model_response}
+    def get_simple_bot_node(self, state:SimpleState):
+        model_response = self.model.invoke(state['messages'])
+        return {'messages': [model_response]}
            
