011d6f856553621b2f5f9f6278a4bf0fae19f943
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-04-03 22:38:16 +0530

    Tools class is defined

--- Web_Search_Functionality_Chatbot/src/web_search_agentic_bot/tools/web_search_tools.py
+++ Web_Search_Functionality_Chatbot/src/web_search_agentic_bot/tools/web_search_tools.py
@@ -1,1 +1,17 @@
 from langchain_tavily import TavilySearch
+from langgraph.prebuilt import ToolNode
+
+class ToolsNode:
+
+    def __init__(self, user_controls):
+        self.user_controls = user_controls
+
+    def get_bind_tools(self):
+
+        tavily_tool = TavilySearch(max_serach=5, topic="general", search_depth="basic")
+        tools=[tavily_tool]
+        return tools
+    
+    def get_tool_node(self):
+        return ToolNode(self.get_bind_tools())
+
