3bb4fc8f98ec33f8962cef77271e1a2dd98462ac
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-04-05 16:37:11 +0530

    language blog generation graph builder design

--- Blog_Generation_With_Language_Translation/src/blog_generation_bot/graphs/blog_graph_builder.py
+++ Blog_Generation_With_Language_Translation/src/blog_generation_bot/graphs/blog_graph_builder.py
@@ -24,10 +24,36 @@ class BlogGraphBuilder:
 
         return self.graph
     
+    def get_language_blog_graph_builder(self):
+
+        blog_nodes_obj = BlogNodes(llm=self.llm)
+        #adding nodes
+
+        self.graph.add_node("title_creation_node",blog_nodes_obj.title_creation_node)
+        self.graph.add_node("content_generation_node",blog_nodes_obj.content_generation_node)
+        self.graph.add_node("language_router", )
+        self.graph.add_node("hindi_translator",)
+        self.graph.add_node("telugu_translator",)
+
+        #adding edges
+
+        self.graph.add_edge(START, "title_creation_node")
+        self.graph.add_edge("title_creation_node", "content_generation_node")
+        self.graph.add_edge("content_generation_node", "language_router")
+        self.graph.add_conditional_edges("language_router", self.blog_nodes_obj.route_decision_maker, 
+                                         {"hindi":"hindi_translator",
+                                          "telugu":"telugu_translator"})
+        self.graph.add_edge("hindi_translator", END)
+        self.graph.add_edge("telugu_translator", END)
+
+    
     def get_setup_graph(self, usecase):
 
         if usecase=="topic":
             return self.get_blog_graph_builder().compile()
+        
+        elif usecase=="language":
+            return self.get_language_blog_graph_builder().compile()
         
 
 #below code is for langgraph studio
@@ -36,8 +62,8 @@ from src.blog_generation_bot.llms.groq_model import GroqModel
 
 llm = GroqModel().get_groq_model()
 graph_builder = BlogGraphBuilder(llm=llm)
-blog_agent = graph_builder.get_blog_graph_builder().compile()
-
+# blog_agent = graph_builder.get_blog_graph_builder().compile()
+lang_blog_agent = graph_builder.get_language_blog_graph_builder().compile()
 
 
 
